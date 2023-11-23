81a8b30a2524a3e92b4c65f9cf5332a6f452d38e
chebishev <atanas.chebishev@gmail.com>
2023-11-23 15:54:08 +0200

    Lab: Objects and Classes 6/9 completed

--- 20231115_Objects_And_Classes_Lab/meetings.js
+++ 20231115_Objects_And_Classes_Lab/meetings.js
@@ -1,14 +1,33 @@
 function solve(input){
     let meetings = {};
+    while (input.length > 0) {
+        let [day, name] = input.shift().split(' ');
+        if (!meetings.hasOwnProperty(day)) {
+            meetings[day] = name;
+            console.log(`Scheduled for ${day}`);
+        }
+        else {
+            console.log(`Conflict on ${day}!`);
+        }
+    }
     
+    for (let [key, value] of Object.entries(meetings)) {
+        if (value === false) {
+           continue
+        }
+        console.log(`${key} -> ${value}`);
+
+    }    
+
 }
 solve(['Monday Peter',
 'Wednesday Bill',
 'Monday Tim',
 'Friday Tim']
 )
-solve(['Monday Peter',
-'Wednesday Bill',
-'Monday Tim',
-'Friday Tim']
+solve(['Friday Bob',
+'Saturday Ted',
+'Monday Bill',
+'Monday John',
+'Wednesday George']
 )
